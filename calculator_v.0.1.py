fbaf29be5a84660313b2f075e43c62f9fb5ab720
Lukas Civan <lukas.civan@gmail.com>
2023-02-17 14:16:57 +0900

    Create calculator_v.0.1.py
    This the first version of the program. It asks for an operation (addition, subtraction, multiplication, and division). Changes responses to integers.
    
    Improvements for next version:
    Run multiple times using a while-loop

---
+++ calculator_v.0.1.py
@@ -0,0 +1,20 @@
+print("Welcome to Lukas' calculator!")
+def calculator():
+    operation = input("Would you like to add, subtract, multiply, or divide?: ")
+    operation = operation.upper()
+    int_1 = input("What is the first integer you would like to input?: ")
+    int_1 = int(int_1)
+    int_2 = input("What is the second integer you would like to input?: ")
+    int_2 = int(int_2)
+    if operation == "ADD":
+        print(int_1+int_2)
+    elif operation == "SUBTRACT":
+        print(int_1-int_2)
+    elif operation == "MULTIPLY":
+        print(int_1*int_2)
+    elif operation == "DIVIDE":
+        print(int_1/int_2)
+    else:
+        print("I can't accept that input")
+calculator()
+print("Thank you for using Lukas' calculator")
